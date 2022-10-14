911bfe571f4a1cccf73717885d49cfe0ffd874e6
Owen Newlove <owen.newlove@gmail.com>
2022-10-14 16:52:14 -0400

    changed the word project to post in various places to make the code easier to understand

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
