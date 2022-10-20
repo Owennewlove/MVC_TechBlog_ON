6a3c43635f278990f268f92bd5aa0398e2d997b0
Owen Newlove <owen.newlove@gmail.com>
2022-10-20 17:13:43 -0400

    could not fix the comment handlebars.

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,6 +21,30 @@ const newFormHandler = async (event) => {
   }
 };
 
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#comment-name').value.trim();
+  const description = document.querySelector('#comment-desc').value.trim();
+
+  if (name && description) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ name, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
@@ -33,6 +57,22 @@ const delButtonHandler = async (event) => {
       document.location.replace('/profile');
     } else {
       alert('Failed to delete post');
+    }
+  }
+};
+
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('dataId')) {
+    const id = event.target.getAttribute('dataId');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to update post');
     }
   }
 };
